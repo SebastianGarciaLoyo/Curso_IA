4e1c2ce9a5860e94a73f93ab3143fe6dd76f0a05
SebastianGarciaLoyo <est.sloyo@smart.edu.co>
2025-12-04 16:03:19 -0500

    Cambios finales

--- CursoIA/Dia1/Ejercicio/APIRickYMorty/script.js
+++ CursoIA/Dia1/Ejercicio/APIRickYMorty/script.js
@@ -1,29 +1,34 @@
-const input = document.querySelector('.input1')
-const boton = document.querySelector('.buscar1')
-const container = document.querySelector('.contenedor')
+const input = document.querySelector('.input1');
+const boton = document.querySelector('.buscar1');
+const container = document.querySelector('.contenedor');
 
-boton.addEventListener('click', () =>{
+boton.addEventListener('click', () => {
     const nombre = input.value.trim();
 
     if (nombre === "") {
-        alert("Por favor escriba un nombre.")
+        alert("Por favor escribe un nombre.");
         return;
     }
 
-    fetch('https://rickandmortyapi.com/api/characters/?name=${nombre}')
-    .then(res => res.json())
-    .then(data => {
-        container.innerHTML = "";
+    fetch(`https://rickandmortyapi.com/api/character/?name=${nombre}`)
+        .then(res => res.json())
+        .then(data => {
+            container.innerHTML = ""; // Limpiar resultados anteriores
 
-        if (data.error){
-            container.innerHTML = '<h2>No se pudo encontrar el personaje</h2>'
-            return;
-        }
+            if (data.error) {
+                container.innerHTML = `<h2>No se encontró el personaje</h2>`;
+                return;
+            }
 
-        data.results.forEach(personaje => {
-            container.innerHTML += '<div class= "carta"> <img src= ${personaje.image}" alt="${personaje.name}"> <h3> ${personaje.name}</h3> <p><strong>Estado: </strong> ${personaje.status}</p>';
-            
-        });
-    })
-    .catch(err => console.log(err))
-})
+            data.results.forEach(personaje => {
+                container.innerHTML += `
+                    <div class="card">
+                        <img src="${personaje.image}" alt="${personaje.name}">
+                        <h3>${personaje.name}</h3>
+                        <p><strong>Estado:</strong> ${personaje.status}</
+                    <>
+                `;
+            });
+        })
+        .catch(err => console.log(err));
+});
